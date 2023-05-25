a80d1cc2249d80fe8ea0ae411b6260d8cc15963a
pranavbadhan8 <120732731+pranavbadhan8@users.noreply.github.com>
2023-05-25 15:13:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+function playSound(soundFile) {
+  const audioElement = document.getElementById('audio');
+
+  // Clear any existing source elements
+  while (audioElement.firstChild) {
+    audioElement.removeChild(audioElement.firstChild);
+  }
+
+  const sourceElement = document.createElement('source');
+  sourceElement.src = `sounds/${soundFile}`;
+  audioElement.appendChild(sourceElement);
+
+  audioElement.load();
+  audioElement.play()
+    .catch((error) => {
+      console.error(error);
+      alert('Failed to play the audio. Please check the file format and location.');
+    });
+}
+
+function stopSound() {
+  const audioElement = document.getElementById('audio');
+  audioElement.pause();
+  audioElement.currentTime = 0;
+}
