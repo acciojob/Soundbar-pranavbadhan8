9e4fc57d1ef226e11208bc23f050c01d87ecd35b
pranavbadhan8 <120732731+pranavbadhan8@users.noreply.github.com>
2023-05-25 15:18:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
 //your JS code here. If required.
-function playSound(soundFile) {
-  const audioElement = document.getElementById('audio');
+const audio = document.getElementById("audio");
 
-  // Clear any existing source elements
-  while (audioElement.firstChild) {
-    audioElement.removeChild(audioElement.firstChild);
-  }
-
-  const sourceElement = document.createElement('source');
-  sourceElement.src = `sounds/${soundFile}`;
-  audioElement.appendChild(sourceElement);
-
-  audioElement.load();
-  audioElement.play()
-    .catch((error) => {
-      console.error(error);
-      alert('Failed to play the audio. Please check the file format and location.');
-    });
+function play(src){
+	audio.src=src;
+	audio.play();
 }
 
-function stopSound() {
-  const audioElement = document.getElementById('audio');
-  audioElement.pause();
-  audioElement.currentTime = 0;
+function stop(){
+	audio.pause();
 }
